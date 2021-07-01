36919db72aac75dd02d3807f59125299872bde66
dziubichmarlena <dziubichmarlena@gmail.com>
2021-07-01 20:19:13 +0200

    range.js file added

---
+++ range.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function range(start, end) {
+    if (end !== undefined) {
+        let result = [];
+        for (let i = start; i <= end; i++)
+            result.push(i)
+        return result;
+    } else {
+        return function(end) {
+            let result = [];
+            for (let i = start; i <= end; i++)
+                result.push(i)
+            return result;
+        }
+    }
+}
+
+console.log(range(3, 3)); // [3]
+console.log(range(3, 8)); // [3,4,5,6,7,8]
+console.log(range(3, 0)); // []
+
+var start3 = range(3);
+var start4 = range(4);
+
+console.log(start3(3)); // [3]
+console.log(start3(8)); // [3,4,5,6,7,8]
+console.log(start3(0)); // []
+
+console.log(start4(6)); // [4,5,6]
